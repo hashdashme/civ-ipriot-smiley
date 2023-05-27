2e3009f49cdd0cda68fa2651ab6cdcf2172119dd
Oscar <>
2023-05-27 20:46:49 +0800

    Sad can blink!

--- sad.py
+++ sad.py
@@ -1,5 +1,5 @@
 from smiley import Smiley
-
+import time
 
 class Sad(Smiley):
     def __init__(self):
@@ -24,3 +24,17 @@ class Sad(Smiley):
         eyes = [10, 13, 18, 21]
         for pixel in eyes:
             self.pixels[pixel] = self.BLANK if wide_open else self.YELLOW
+    
+    def blink(self, delay=0.25):
+        """
+        Make the sad smiley blink once with a certain delay (in s).
+        This is the implementation of the abstract method from the
+        Blinkable abstract class.
+
+        :param delay: Delay in seconds
+        """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
